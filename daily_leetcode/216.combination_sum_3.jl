7cc44dcca24a19981b2fd27087773f62d7046e02
BangBOOM <looplearngotoloop@gmail.com>
2022-05-10 23:54:42 +0800

    solved 216

---
+++ daily_leetcode/216.combination_sum_3.jl
@@ -0,0 +1,27 @@
+using Test
+
+function combination_sum(k::Int, n::Int)::Vector{Vector{Int}}
+    (n<sum(1:k) || n>sum(9:-1:10-k)) && return []
+    q1 = [[i] for i in 1:10-k]
+    q2 = []
+
+    for i in 1:k-1
+        while ! isempty(q1)
+            x = popfirst!(q1)
+            append!(
+                q2,
+                [x...,j] for j in x[end]+1:10-k+i
+            )
+        end
+        q1 = q2
+        q2 = []
+    end
+    filter(x->sum(x)==n,q1)
+end
+
+
+@testset "216.combination_sum_3.jl" begin
+    @test combination_sum(3,7) == [[1,2,4]]
+    @test combination_sum(3,9) == [[1,2,6],[1,3,5],[2,3,4]]
+    @test combination_sum(4,1) == []
+end
